ad2c412890c068f8f8c44ff698577a80aded2e35
AadityaMohit <150881374+AadityaMohit@users.noreply.github.com>
2024-09-24 18:35:26 +0530

    Add About.css file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,30 @@
-// Navbar.js
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../App.css';
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav className="navbar">
+    <nav className="news-navbar">
       <div className="navbar__brand">React App</div>
-      {/* <div><Link className="navbar__brand" to='/'>React App </Link></div>  */}
-      {/* <div><Link className="navbar__brand" to='/weather'>Weather_Now</Link></div>  */}
-      <ul className="navbar__links">
-        
+      <div className={`navbar__links ${isOpen ? 'active' : ''}`}>
         <li><Link className="navbar__link_text" to='/weather'>Weather_Now</Link></li>
-        <li className="navbar__link">
-          <Link to="/about" className="navbar__link__text">About</Link>
-        </li>
-        <li className="navbar__link">
-          <Link to="/" className="navbar__link__text">Home</Link>
-        </li>
-        <li className="navbar__link">
-          <Link to="/contact" className="navbar__link__text">Contact</Link>
-        </li>
-<li><Link className="navbar__link" to='/news'>News_Now</Link></li>
-        <li className="navbar__link">
-          <Link to="/signup" className="navbar__link__text">Sign Up</Link>
-        </li>
-        <li className="navbar__link">
-          <Link to="/login" className="navbar__link__text">Log In</Link>
-        </li>
-      </ul>
+        <li className="navbar__link"><Link to="/about" className="navbar__link__text">About</Link></li>
+        <li className="navbar__link"><Link to="/" className="navbar__link__text">Home</Link></li>
+        <li className="navbar__link"><Link to="/contact" className="navbar__link__text">Contact</Link></li>
+        <li><Link className="navbar__link" to='/news'>News_Now</Link></li>
+
+      </div>
+      <div className="hamburger" onClick={toggleMenu}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </nav>
   );
 }
