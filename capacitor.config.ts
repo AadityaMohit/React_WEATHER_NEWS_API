1cc3f013c6629eb4f0a8770167f9eed719be4ea6
AadityaMohit <150881374+AadityaMohit@users.noreply.github.com>
2024-09-29 13:11:16 +0530

    Update Navbar component and add SignUp component

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.weather.app',
+  appName: 'AadityMohi App',
+  webDir: 'build'
+};
+
+export default config;
