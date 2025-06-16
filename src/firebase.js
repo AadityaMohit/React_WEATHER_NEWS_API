af939a4b9a14aae2e53ac26dc07f656dbe07251a
AadityaMohit <150881374+AadityaMohit@users.noreply.github.com>
2025-06-16 15:28:50 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = {
-    apiKey: "AIzaSyAK3UIwUUwLLCGswz9lO90ICDviXmkOD2I",
-    authDomain: "testextension-1e32b.firebaseapp.com",
-    projectId: "testextension-1e32b",
-    storageBucket: "testextension-1e32b.appspot.com",
-    messagingSenderId: "736473904434",
-    appId: "1:736473904434:web:77259b6327747127b9d32b",
-    measurementId: "G-ZY1EZREYB3"
+    apiKey: " ",
+    authDomain: " ",
+    projectId: " ",
+    storageBucket: " ",
+    messagingSenderId: " ",
+    appId: " ",
+    measurementId: " "
   };
 
 
